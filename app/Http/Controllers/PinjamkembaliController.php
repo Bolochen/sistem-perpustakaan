e4e3271f3ab72cf82f3406c0ac21db1a4dc55bb4
Bolo Chen <bolosstuff12@gmail.com>
2023-03-10 10:50:12 +0700

    membuat model dan factory

---
+++ app/Http/Controllers/PinjamkembaliController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pinjamkembali;
+use App\Http\Requests\StorePinjamkembaliRequest;
+use App\Http\Requests\UpdatePinjamkembaliRequest;
+
+class PinjamkembaliController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePinjamkembaliRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pinjamkembali $pinjamkembali)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pinjamkembali $pinjamkembali)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePinjamkembaliRequest $request, Pinjamkembali $pinjamkembali)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pinjamkembali $pinjamkembali)
+    {
+        //
+    }
+}
